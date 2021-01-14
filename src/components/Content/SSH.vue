60e2b0dea2e4f36411c79f65a63302e2713b41f1
phhz <18851908011@qq.com>
2021-01-14 15:07:10 +0800

    修改笔记SSH

--- src/components/Content/SSH.vue
+++ src/components/Content/SSH.vue
@@ -8,12 +8,16 @@
          <pre v-show="isShow"  style="word-wrap: break-word; white-space: pre-wrap;">
         <a href="https://gitee.com/help/articles/4181#article-header0">SSH公钥生成教程</a>
             1.ssh-keygen -t rsa -C "xxxxx@xxxxx.com" （改！）(在随便一个本地文件夹打开git终端输)
-            三次回车，本地c盘的用户xx里有了一个.ssh文件夹
+            秘钥改名字，如/c/Users/平和猴子/.ssh/pphhz，本地c盘的用户平和猴子里有了一个.ssh文件夹
 
-            2.cat ~/.ssh/id_rsa.pub
+            2.cat ~/.ssh/pphhz.pub
                 拷贝公钥(别拷多)
 
-            3.github-->setting-->SSH and GPG keys-->new SSH key-->粘贴保存
+            3.github账号和本地关联：
+             github-->setting-->SSH and GPG keys-->new SSH key-->粘贴保存
+             （这样就不用一个一个库授权，相当于是自己）
+             别人的私密库要给我权限：
+             别人的github-->一个库-->Deploy keys-->粘贴保存
 
             4.ssh -T git@gitee.com（改成github.com）
                 yes
@@ -38,7 +42,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss" scoped> 
+<style scoped lang="scss" scoped>
 @import '../../assets/css/variable';
  a{
        color:$font-color2;
@@ -55,8 +59,8 @@ export default {
     background-color: $background-color2;
     cursor: pointer;
 }
-  
- 
+
+
   pre{
     margin: 10px;
   }
