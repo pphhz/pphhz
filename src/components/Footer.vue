16533cc43793706241e45a5750b88dadf79a3f31
phhz <18851908011@qq.com>
2021-07-02 21:45:04 +0800

    添加top按钮

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="footer">
-    <div class="footer-right">
-      <div style="position: relative;">
-        <a href="#" target="_self" style="color:whitesmoke;position: absolute;left: 16px;top: 15px;">^</a>
+    <div class="footer-right" v-show="goTopShow">
+      <div style="position: relative;cursor: default;width: 100%;height: 100%;" @click="goTop">
+        <span style="color:whitesmoke;position: absolute;left: 16px;top: 15px;">^</span>
       </div>
     </div>
 
@@ -17,10 +17,52 @@ export default {
   },
   data(){
     return{
+      scrollTop: "",
+      goTopShow: false
     }
   },
-  methods:{
-  },
+  watch: {
+            scrollTop(val) {
+               if (this.scrollTop > 500) {
+                   this.goTopShow = true;
+                } else {
+                     this.goTopShow = false;
+                 }
+          }
+      },
+       methods: {
+          handleScroll() {
+                 this.scrollTop =
+                         window.pageYOffset ||
+                   document.documentElement.scrollTop ||
+                     document.body.scrollTop;
+                if (this.scrollTop > 500) {
+                      this.goTopShow = true;
+                   }
+            },
+           goTop() {
+               let timer = null,
+                            _that = this;
+                cancelAnimationFrame(timer);
+                 timer = requestAnimationFrame(function fn() {
+                     if (_that.scrollTop > 0) {
+                             _that.scrollTop -= 250;
+                             document.body.scrollTop = document.documentElement.scrollTop =
+                                      _that.scrollTop;
+                             timer = requestAnimationFrame(fn);
+                         } else {
+                             cancelAnimationFrame(timer);
+                             _that.goTopShow = false;
+                       }
+                 });
+             }
+      },
+     mounted() {
+           window.addEventListener("scroll", this.handleScroll);
+      },
+      destroyed() {
+           window.removeEventListener("scroll", this.handleScroll);
+      }
 }
 </script>
 
