d296b030ecdf13848b8dcc56e81aa674868e7e1d
phhz <18851908011@qq.com>
2021-07-02 21:36:01 +0800

    添加top按钮

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <Header></Header>
     <router-view></router-view>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 
 export default {
   name:"App",
   components:{
     Header,
+    Footer,
   },
   mounted(){
     document.getElementById('app').style.display = 'block';
---
+++ src/components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="footer">
+    <div class="footer-right">
+      <div style="position: relative;">
+        <a href="#" target="_self" style="color:whitesmoke;position: absolute;left: 16px;top: 15px;">^</a>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  components: {
+    // Login
+  },
+  data(){
+    return{
+    }
+  },
+  methods:{
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.footer-right{
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  position:fixed;
+  right: 20px;
+  bottom: 20px;
+  background-color: rgba(0,0,0,0.5);
+}
+</style>
