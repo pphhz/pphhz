8c93bd0882961d58462c519895304f328f1130ed
phhz <18851908011@qq.com>
2021-07-07 13:17:46 +0800

    美化，点击固定变换背景颜色

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,96 +1,118 @@
 <template>
-  <div class="header">
-    <div class="header-left">
-      <ul>
-        <router-link tag="li" to="/home" class="item">
-            <span>主页🏠</span>
-        </router-link>
-        <router-link tag="li" to="/life" class="item">
-            <span>我的画🎨</span>
-        </router-link>
-        <router-link tag="li" to="/Guitar" class="item">
-            <span>吉他🎸</span>
-        </router-link>
-        <router-link tag="li" to="/support" class="item">
-            <span>支持我💰</span>
-        </router-link>
-        <router-link tag="li" to="/about" class="item">
-            <span>关于❔</span>
-        </router-link>
-      </ul>
+    <div class="header">
+        <div class="header-left">
+            <ul v-for="(item,index) in dict">
+                <router-link tag="li" :to="item.path" class="item" :class='{bg:index==activeIndex}'>
+                    <div @click="bgcChange(index)">{{item.discp}}</div>
+                </router-link>
+            </ul>
+
+        </div>
+        <div class="header-right">
+            <ul>
+                <li v-show="show">3045078817</li>
+                <li @click="toggle" class="item">平和猴子🐧</li>
+                <!--        <li><button href="" @click="changeIsShowLogin">登录/注册</button></li>-->
+            </ul>
+        </div>
+        <!--    <Login v-model="isShowLogin" @balabala="queryFunction()"/>-->
 
     </div>
-    <div class="header-right">
-      <ul>
-        <li v-show="show">3045078817</li>
-        <li @click="toggle" class="item">平和猴子🐧</li>
-<!--        <li><button href="" @click="changeIsShowLogin">登录/注册</button></li>-->
-      </ul>
-    </div>
-<!--    <Login v-model="isShowLogin" @balabala="queryFunction()"/>-->
-
-  </div>
 </template>
 
 <script>
-// import Login from '@/views/Login'
-export default {
-  name: 'Header',
-  components: {
-    // Login
-  },
-  data(){
-    return{
-      show:false,
-      isShowLogin:false,
+    // import Login from '@/views/Login'
+    export default {
+        name: 'Header',
+        components: {
+            // Login
+        },
+        data() {
+            return {
+                show: false,
+                isShowLogin: false,
+                activeIndex: 0,
+                dict:
+                    [
+                        {
+                            path: '/home',
+                            discp: '主页🏠'
+                        },
+                        {
+                            path: '/life',
+                            discp: '我的画🎨'
+                        },
+                        {
+                            path: '/Guitar',
+                            discp: '吉他🎸'
+                        },
+                        {
+                            path: '/support',
+                            discp: '支持我💰'
+                        },
+                        {
+                            path: '/about',
+                            discp: '关于❔'
+                        },
+                    ]
+            }
+        },
+        methods: {
+            bgcChange(index) {
+                this.activeIndex = index
+            },
+            toggle() {
+                this.show = !this.show;
+            },
+            changeIsShowLogin() {
+                this.isShowLogin = true;
+            },
+            queryFunction() {
+                this.isShowLogin = false;
+            }
+        },
     }
-  },
-  methods:{
-    toggle(){
-      this.show=!this.show;
-    },
-    changeIsShowLogin(){
-      this.isShowLogin=true;
-    },
-    queryFunction(){
-      this.isShowLogin=false;
-    }
-  },
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-@import '../assets/css/variable';
-.item{
-  //改鼠标为小手
-  cursor:pointer;
-  &:hover{
-    background-color: $background-color4;
-  }
-}
-.header{
-  width: 100%;
-  position:fixed;
-  top: 0;
-  left: 0;
-  display: flex;
-  justify-content: space-between;
-  border-bottom: 1px solid $border-color;
-  background: $background-color;
-  z-index: 999;
-    ul{
-      li{
-        width: 100px;
-        line-height: 39px;
-        float: left;
-        text-align: center;
-        font-size: $font_medium;
-        color:$font-color;
-      }
-  }
-  .header-right{
-    margin-right: 20px;
-  }
-}
+    @import '../assets/css/variable';
+
+    .item {
+        //改鼠标为小手
+        cursor: pointer;
+    }
+
+    .bg {
+        background-color: $background-color4;
+    }
+
+    .header {
+        width: 100%;
+        position: fixed;
+        top: 0;
+        left: 0;
+        display: flex;
+        justify-content: space-between;
+        border-bottom: 1px solid $border-color;
+        background: $background-color;
+        z-index: 999;
+
+        ul {
+            display: inline;
+
+            li {
+                width: 100px;
+                line-height: 39px;
+                float: left;
+                text-align: center;
+                font-size: $font_medium;
+                color: $font-color;
+            }
+        }
+
+        .header-right {
+            margin-right: 20px;
+        }
+    }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,20 @@
 <template>
-  <div class="home">
-      <div class="home-middle">
-        <div class="warpper">
-            <router-view></router-view>
+    <div class="home">
+        <div class="home-middle">
+            <div class="warpper">
+                <router-view></router-view>
+            </div>
         </div>
-      </div>
-      <div class="home-left">
-        <ul>
-          <router-link tag="li" to="/home/htmlCss" class="item">
-            <span>Html、Css📓</span>
-          </router-link>
-            <router-link tag="li" to="/home/js" class="item">
-                <span>JavaScript📓</span>
-            </router-link>
-          <router-link tag="li" to="/home/web" class="item">
-            <span>前端学习网站💻</span>
-          </router-link>
-           <router-link tag="li" to="/home/plugIn" class="item">
-            <span>插件、库💻</span>
-          </router-link>
-          <router-link tag="li" to="/home/buildSite" class="item">
-            <span>搭建网页💻</span>
-          </router-link>
-          <router-link tag="li" to="/home/interview" class="item">
-            <span>就业面试🙎</span>
-          </router-link>
-          <router-link tag="li" to="/home/note" class="item">
-            <span>随笔📜</span>
-          </router-link>
-          <router-link tag="li" to="/home/website" class="item">
-            <span>生活干货网站📺</span>
-          </router-link>
-        </ul>
-      </div>
+        <div class="home-left">
+            <ul v-for="(item,index) in dict">
+                <router-link tag="li" :to="item.path" class="item" :class='{bg:index==activeIndex}'>
+                    <div @click="bgcChange(index)">{{item.discp}}</div>
+                </router-link>
+            </ul>
+        </div>
 
-      <div class="home-right">
-        <h3>平和猴子的兴趣</h3>
+        <div class="home-right">
+            <h3>平和猴子的兴趣</h3>
             <p>前端</p>
             <p>美剧日剧等</p>
             <p>动漫</p>
@@ -43,95 +22,150 @@
             <p>画画</p>
             <p>吉他</p>
             <p>单机剧情类游戏</p>
-          <h3 style="margin-top: 40px;">我画的微信表情包：</h3>
-          <img src="../assets/images/qrCode.jpg" alt="" style="width: 100px;margin-top: 20px;">
-          <h3 style="margin-top: 40px;">友情链接：（我参与制作）</h3>
-              <p style="margin-top: 10px;"><a href="https://www.reol.fans/">Reol粉丝网站</a></p>
-              <p><a href="https://reol.work/">Reol2019生贺网站</a></p>
-      </div>
-  </div>
+            <h3 style="margin-top: 40px;">我画的微信表情包：</h3>
+            <img src="../assets/images/qrCode.jpg" alt="" style="width: 100px;margin-top: 20px;">
+            <h3 style="margin-top: 40px;">友情链接：（我参与制作）</h3>
+            <p style="margin-top: 10px;"><a href="https://www.reol.fans/">Reol粉丝网站</a></p>
+            <p><a href="https://reol.work/">Reol2019生贺网站</a></p>
+        </div>
+    </div>
 </template>
 
 <script>
 
-export default {
-  name:"Home",
-  components:{
-
-},
-
-
-}
+    export default {
+        name: "Home",
+        components: {},
+        data() {
+            return {
+                activeIndex: 0,
+                dict:
+                    [
+                        {
+                            path: '/home/htmlCss',
+                            discp: 'Html、Css📓'
+                        },
+                        {
+                            path: '/home/js',
+                            discp: 'JavaScript📓'
+                        },
+                        {
+                            path: '/home/buildSite',
+                            discp: '搭建网页📜'
+                        },
+                        {
+                            path: '/home/note',
+                            discp: '随笔📜'
+                        },
+                        {
+                            path: '/home/interview',
+                            discp: '就业面试🙎'
+                        },
+                        {
+                            path: '/home/plugIn',
+                            discp: '插件、库💻'
+                        },
+                        {
+                            path: '/home/web',
+                            discp: '前端学习网站💻'
+                        },
+                        {
+                            path: '/home/website',
+                            discp: '生活干货网站📺'
+                        }
+                    ]
+            }
+        },
+        methods: {
+            bgcChange(index) {
+                this.activeIndex = index
+            }
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
-@import "../assets/css/variable";
-a{
-    color:$font-color2;
-    &:hover{
-        color:$font-active-color-theme1;
-    }
-}
-.item{
-  //改鼠标为小手
-  cursor:pointer;
-  &:hover{
-    background-color: $background-color3;
-  }
-}
-.home{
-  width: 100%;
-  /*height: 500px;*/
-  color:$font-color1;
-  font-size: $font_medium;
-  .home-left{
-      width: 250px;
-      height: 100%;
-      // margin-left: -100%;
-      box-sizing: border-box;
-      float: left;
-      text-align: center;
-      background-color:$background-color1;
-      border-bottom-right-radius:10px;
-      position:fixed;
-      left: 0;
-      top: 40px;
-      ul{
-        li{
-          line-height: 40px;
-          border-bottom: 1px solid $border-color1;
+    @import "../assets/css/variable";
+
+    a {
+        color: $font-color2;
+
+        &:hover {
+            color: $font-active-color-theme1;
         }
-      }
-  }
-  .home-middle{
-        width: 100%;
-        float: left;
-        color:$font-color3;
-        font-size: $font_large;
-
-      .warpper{
-        background: rgb(148, 200, 221);
-        margin: 40px 250px 0 250px;
-
-      }
-  }
-  .home-right{
-      width: 250px;
-      height: 100%;
-      // margin-left: -250px;
-      float: left;
-      padding: 15px;
-      box-sizing: border-box;
-      background-color:$background-color1;
-      border-bottom-left-radius:10px;
-      position:fixed;
-      right: 0;
-      top: 40px;
-      p:first-of-type{
-        margin-top: 20px;
     }
-  }
 
-}
+    .bg {
+        background-color: $background-color3;
+    }
+
+    .item {
+        //改鼠标为小手
+        cursor: pointer;
+    }
+
+    .home {
+        width: 100%;
+        /*height: 500px;*/
+        color: $font-color1;
+        font-size: $font_medium;
+
+        .home-left {
+            width: 250px;
+            height: 100%;
+            // margin-left: -100%;
+            box-sizing: border-box;
+            float: left;
+            text-align: center;
+            background-color: $background-color1;
+            border-bottom-right-radius: 10px;
+            position: fixed;
+            left: 0;
+            top: 40px;
+
+            ul {
+                li {
+                    line-height: 40px;
+                    border-bottom: 1px solid $border-color1;
+
+                    div {
+                        width: 100%;
+                    }
+                }
+            }
+        }
+
+        .home-middle {
+            width: 100%;
+            float: left;
+            color: $font-color3;
+            font-size: $font_large;
+
+            .warpper {
+                background: rgb(148, 200, 221);
+                margin: 40px 250px 0 250px;
+
+            }
+        }
+
+        .home-right {
+            width: 250px;
+            height: 100%;
+            // margin-left: -250px;
+            float: left;
+            padding: 15px;
+            box-sizing: border-box;
+            background-color: $background-color1;
+            border-bottom-left-radius: 10px;
+            position: fixed;
+            right: 0;
+            top: 40px;
+
+            p:first-of-type {
+                margin-top: 20px;
+            }
+        }
+
+    }
 
 </style>
